c20585ee4b31afe2a1365d433f3c0a5b216edae8
j-mattias <mattiasjohansson.vfx@gmail.com>
2024-07-12 08:23:31 +0200

    Added another solution for 13 comparing against a correct array of objects

--- wrong.js
+++ wrong.js
@@ -71,3 +71,57 @@ function editInstructions(arr, index, text) {
   }
   arr[index].innerHTML = text;
 }
+
+// Solution assuming there was an array with the correct instructions
+const instructionSteps = [
+  {
+    order: 1,
+    text: "Smula sönder 10 stycken digistivetex och blanda sen med smöret. Bred ut det i botten av en en rund form med lösbara kanter. Det ska bli som en tjockt täcke på botten av formen.",
+  },
+  {
+    order: 2,
+    text: "Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke.",
+  },
+  {
+    order: 3,
+    text: "Vispa äggvitorna hårt med en elvisp. De är tillräckligt vispade när du kan vända uppochner på bunken utan att det rör sig.",
+  },
+  {
+    order: 4,
+    text: "Häll upp grädden i en mellanstor bunke. Vispa de relativ hårt. En aning hårdare en vanligt.",
+  },
+  {
+    order: 5,
+    text: "Blanda i philadelphiaosten och vaniljsockret med äggulorna och vispa ihop.",
+  },
+  {
+    order: 6,
+    text: "Häll nu över innehållet i bunken med äggulor över grädden. Vispa ihop på lägsta hastigheten.",
+  },
+  {
+    order: 7,
+    text: "Häll sedan över det innehållet till bunken med äggvitorna. Blande ihop med en gaffel tills det blir en slät fluffig smet.",
+  },
+  {
+    order: 8,
+    text: "Häll smeten i formen, smula över de resterande digistivetexen om du vill.",
+  },
+  {
+    order: 9,
+    text: "Ställ in i frysen över natten.",
+  },
+  {
+    order: 10,
+    text: "Servera och toppa med det du känner för.",
+  },
+];
+
+editInstructionsCompare(instructionsListRef, instructionSteps);
+
+function editInstructionsCompare(wrongArr, correctArr) {
+  wrongArr.forEach((elem, index) => {
+    if (elem.innerText !== correctArr[index].text) {
+      elem.innerText = correctArr[index].text;
+    }
+  });
+}
